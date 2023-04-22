cdf78fe3abf827226e3e61423356e1bf61d64338
Georgios Vlassis <gvlassis@mailbox.org>
2023-04-22 16:07:56 +0300

    Added dependencies provision, replaced relative paths with absolute ones and some other minor fixes

--- src/pokecat.sh
+++ src/pokecat.sh
@@ -2,14 +2,38 @@
 # SPDX-FileCopyrightText: © 2023 Project's authors 
 # SPDX-License-Identifier: MIT
 
-src_path="$(dirname ${0})"
-res_path="${src_path}/../res"
+dependencies="catimg"
+missing_dependencies=""
+for dependency in ${dependencies}; do
+    if ! command -v "${dependency}" &>/dev/null; then
+        if [ -n "${missing_dependencies}" ]; then
+            missing_dependencies="${missing_dependencies}, ${dependency}"
+        else
+            missing_dependencies="${dependency}"
+        fi
+    fi
+done
 
-pokemon="$(ls ${res_path} | shuf -n 1 | xargs basename -s ".png")"
+if [ -n "${missing_dependencies}" ]; then
+    printf "\e[31;1mError: The following dependencies are not in PATH: ${missing_dependencies}\e[0m"
+    exit 1
+fi
+
+src_path="$(readlink -f "$(dirname "${0}")")"
+res_path="$(readlink -f "$(dirname "${src_path}")")/res"
+if [ ! -d "${res_path}" ]; then
+    printf "\e[31;1mError: ${res_path} is not a directory. Run utils/get_res.sh.\e[0m\n"
+    exit 2
+fi
+
+pokemon="$(ls "${res_path}" | shuf -n 1 | xargs basename -s ".png")"
 
 catimg "${res_path}/${pokemon}.png"
 printf "\e[1m󰐝 ${pokemon}\e[0m\n"
 
+unset dependencies
+unset missing_dependencies
+unset dependency
 unset src_path
 unset res_path
 unset pokemon
--- utils/get_res.sh
+++ utils/get_res.sh
@@ -2,9 +2,26 @@
 # SPDX-FileCopyrightText: © 2023 Project's authors 
 # SPDX-License-Identifier: MIT
 
-utils_path="$(dirname ${0})"
-res_path="${utils_path}/../res"
-svn export https://github.com/PokeAPI/sprites/trunk/sprites/pokemon/versions/generation-iv/platinum "${res_path}"
+dependencies="svn curl jq mogrify"
+missing_dependencies=""
+for dependency in ${dependencies}; do
+    if ! command -v "${dependency}" &>/dev/null; then
+        if [ -n "${missing_dependencies}" ]; then
+            missing_dependencies="${missing_dependencies}, ${dependency}"
+        else
+            missing_dependencies="${dependency}"
+        fi
+    fi
+done
+
+if [ -n "${missing_dependencies}" ]; then
+    printf "\e[31;1mError: The following dependencies are not in PATH: ${missing_dependencies}\e[0m"
+    exit 1
+fi
+
+src_path="$(readlink -f "$(dirname "${0}")")"
+res_path="$(readlink -f "$(dirname "${src_path}")")/res"
+svn export --force https://github.com/PokeAPI/sprites/trunk/sprites/pokemon/versions/generation-iv/platinum "${res_path}"
 rm -rf "${res_path}/back" "${res_path}/shiny" "${res_path}/female"
 
 # Keep only one Unown form
@@ -33,8 +50,8 @@ rm "${res_path}"/492-*
 rm "${res_path}"/493-*
 
 for id in {1..493}; do
-    printf "${id}/493\n"
-    name="$(curl "https://pokeapi.co/api/v2/pokemon/${id}" | jq ".name")"
+    printf "\e[35;1m${id}/493\e[0m\n"
+    name="$(curl "https://pokeapi.co/api/v2/pokemon/${id}" | jq -r ".name")"
     mv "${res_path}/${id}.png" "${res_path}/${id}-${name^}.png"
 done 
 
@@ -45,8 +62,11 @@ mv "${res_path}/487-Giratina-altered.png" "${res_path}/487-Giratina.png"
 mv "${res_path}/492-Shaymin-land.png" "${res_path}/492-Shaymin.png"
 
 # Trim transparency
-mogrify -trim ${res_path}/*
+mogrify -trim "${res_path}/*"
 
+unset dependencies
+unset missing_dependencies
+unset dependency
 unset utils_path
 unset res_path
 unset name
